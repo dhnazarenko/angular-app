42bd5fad588b460449b902e5bb63ac759252f6b1
dhnazarenko <danielnazarenko.de@gmail.com>
2021-08-20 00:20:01 +0200

    :sparkles: add router modul
    ng generate module app-routing --flat --module=app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BookingsComponent } from './bookings/bookings.component';
+
+const routes: Routes = [
+  {
+    path: 'bookings',
+    component: BookingsComponent,
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
