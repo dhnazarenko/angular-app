0f31a03d58a8f7ff03037be56389ab7458290c77
dhnazarenko <danielnazarenko.de@gmail.com>
2021-08-20 00:10:58 +0200

    :wastebasket: add mock data

---
+++ src/app/mock-bookings.ts
@@ -0,0 +1,39 @@
+import { Booking } from './booking';
+
+export const Bookings: Booking[] = [
+  {
+    id: 1,
+    name: ' Daniel Nazarenko',
+    roomNumber: 100,
+    startDate: new Date(),
+    endDate: new Date('2021-07-21'),
+  },
+  {
+    id: 2,
+    name: ' Kevin Perk',
+    roomNumber: 57,
+    startDate: new Date(),
+    endDate: new Date('2021-08-11'),
+  },
+  {
+    id: 3,
+    name: ' Peter Lustig',
+    roomNumber: 133,
+    startDate: new Date(),
+    endDate: new Date('2021-08-25'),
+  },
+  {
+    id: 4,
+    name: ' Max Muster',
+    roomNumber: 87,
+    startDate: new Date(),
+    endDate: new Date('2021-10-15'),
+  },
+  {
+    id: 5,
+    name: ' Gustav Gans',
+    roomNumber: 187,
+    startDate: new Date(),
+    endDate: new Date('2021-12-12'),
+  },
+];
